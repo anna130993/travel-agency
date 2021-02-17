b0cc2fc8233c1c85d0c2724292f2c37bbad16bb2
Anna Podsiadlo <annamagdalena.podsiadlo@gmail.com>
2021-02-17 20:27:56 +0100

    Finish the first testing task

---
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import TripSummary from './TripSummary.js';
+
+describe('Component TripSummary', () => {
+  it ('should generate link to correct address', () => {
+    const expectedId = 'unicorn';
+    const component = shallow(<TripSummary id={expectedId} tags={[]} />);
+    expect (component.find('Link').prop('to')).toEqual('/trip/' + expectedId);
+  });
+  it ('should have correct src and alt for <img>', () => {
+    const expectedSrc = 'img.jpg';
+    const expectedAlt = 'alt';
+    const component = shallow(<TripSummary image={expectedSrc} name={expectedAlt} tags={[]}/>);
+
+    expect (component.find('img').prop('src')).toEqual(expectedSrc);
+    expect (component.find('img').prop('alt')).toEqual(expectedAlt);
+  });
+  it ('should render name, cost and days properly', () => {
+    const expectedName = 'Strange by Celeste';
+    const expectedDays = 14;
+    const expectedCost = '79,99';
+    const component = shallow(<TripSummary name={expectedName} cost={expectedCost} days={expectedDays} tags={[]} />);
+
+    expect (component.find('h3').text()).toEqual(expectedName);
+    expect (component.find('span').first().text()).toEqual(`${expectedDays} days`);
+    expect (component.find('span').at(1).text()).toEqual(`from ${expectedCost}`);
+  });
+  it ('should show error if lacking any of the following - name, cost, days, id  or image', () => {
+    expect (() => shallow(<TripSummary />)).toThrow();
+  });
+  it ('should render span in correct order', () => {
+    const tags = ['storm', 'lightning', 'thunder'];
+    const component = shallow(<TripSummary tags={[...tags]}/>);
+
+    expect(component.find('.tags span').at(0).text()).toEqual(tags[0]);
+    expect(component.find('.tags span').at(1).text()).toEqual(tags[1]);
+    expect(component.find('.tags span').at(2).text()).toEqual(tags[2]);
+  });
+  it ('should crash because of nonexistent tags or empty array', () => {
+    const component = shallow(<TripSummary tags={[]} />);
+
+    expect(component.hasClass('tags')).toBe(false);
+  });
+});
